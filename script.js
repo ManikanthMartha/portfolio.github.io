80b786534af1937b3a3ea4de0b09a570d79144ac
Manikanth Martha <117458699+ManikanthMartha@users.noreply.github.com>
2023-05-11 23:20:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function experience_1() {
+    document.getElementById("experience1").style.visibility = "visible";
+    document.getElementById("experience2").style.visibility = "hidden";
+    document.getElementById("experience3").style.visibility = "hidden";
+    document.getElementById("experience4").style.visibility = "hidden";
+  }
+
+function experience_2() {
+    document.getElementById("experience2").style.visibility = "visible";
+    document.getElementById("experience1").style.visibility = "hidden";
+    document.getElementById("experience3").style.visibility = "hidden";
+    document.getElementById("experience4").style.visibility = "hidden";
+  }
+
+
+  function experience_3() {
+    document.getElementById("experience3").style.visibility = "visible";
+    document.getElementById("experience1").style.visibility = "hidden";
+    document.getElementById("experience2").style.visibility = "hidden";
+    document.getElementById("experience4").style.visibility = "hidden";
+  }
+  function experience_4() {
+    document.getElementById("experience4").style.visibility = "visible";
+    document.getElementById("experience1").style.visibility = "hidden";
+    document.getElementById("experience2").style.visibility = "hidden";
+    document.getElementById("experience3").style.visibility = "hidden";
+  }
